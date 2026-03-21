c9b139c6b9c6d9a1ba7fc8a09a3ddc95c7b190c3
Joel Eckhardt <johardt@proton.me>
2026-03-21 09:51:02 +0100

    Map improvements
    - Make tile selector more efficient
    - Define navigation region

--- scripts/select_tile.gd
+++ scripts/select_tile.gd
@@ -1,10 +1,12 @@
 extends TileMapLayer
 
 @onready var tile_selector: Sprite2D = $"../TileSelector"
+@onready var tilesize: Vector2i = tile_set.tile_size;
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("select tile"):
 		var mouse_pos = get_local_mouse_position()
 		var tile_pos: Vector2i = local_to_map(mouse_pos)
-		var target_pos: Vector2i = tile_pos * tile_set.tile_size + Vector2i(16, 16)
+		@warning_ignore("integer_division")
+		var target_pos: Vector2i = tile_pos * tilesize + (tilesize / 2)
 		tile_selector.position = target_pos
