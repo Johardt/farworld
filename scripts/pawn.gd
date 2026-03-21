3724e1efb1b903fce06e3c0084e669b90a77b6c8
Joel Eckhardt <johardt@proton.me>
2026-03-21 15:10:34 +0100

    Update pawn component
    - Circle collider instead of capsule shape
    - Remove pawn being controllable directly

--- scripts/pawn.gd
+++ scripts/pawn.gd
@@ -1,11 +1,6 @@
 extends StaticBody2D
 
-const SPEED = 1
 
-func _physics_process(delta: float) -> void:
-	var direction_x := Input.get_axis("left", "right")
-	var direction_y := Input.get_axis("up", "down")
-	var speed = SPEED * (delta * 100)
-	
-	var motion = Vector2(direction_x, direction_y).normalized() * speed
-	move_and_collide(motion)
+func _physics_process(_delta: float) -> void:
+	# Use NavigationAgent to move between current position and TileSelector position
+	pass
