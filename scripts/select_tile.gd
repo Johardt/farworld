14d2272a7e4ff84a78df7d352e1a5d73da58575d
Joel Eckhardt <johardt@proton.me>
2026-03-25 16:47:02 +0100

    Improve script stability

--- scripts/select_tile.gd
+++ scripts/select_tile.gd
@@ -5,7 +5,7 @@ extends TileMapLayer
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("select tile"):
-		var mouse_pos = get_local_mouse_position()
+		var mouse_pos: Vector2 = get_local_mouse_position()
 		var tile_pos: Vector2i = local_to_map(mouse_pos)
 		@warning_ignore("integer_division")
 		var target_pos: Vector2i = tile_pos * tilesize + (tilesize / 2)
